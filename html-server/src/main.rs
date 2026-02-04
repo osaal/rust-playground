5d6fdec61ceadde290ee758a7ff2b0c2aa838b7a
osaal <29450366+osaal@users.noreply.github.com>
2026-02-04 19:35:38 +0200

    Add skeleton of functional HTML server

--- html-server/src/main.rs
+++ html-server/src/main.rs
@@ -1,4 +1,29 @@
+use axum::{Json, Router, http::StatusCode, routing::get};
+use serde::Deserialize;
+
 #[tokio::main]
 async fn main() {
-    println!("Hello, world!");
+    let app: Router<()> = Router::new().route("/", get(root).post(root_post));
+    let listener = tokio::net::TcpListener::bind("127.0.0.1:3000").await;
+    if let Ok(res) = listener {
+        axum::serve(res, app).await.unwrap();
+    }
+}
+
+async fn root() -> &'static str {
+    println!("Root requested correctly");
+    "Hello, world!"
+}
+
+async fn root_post(Json(payload): Json<Message>) -> StatusCode {
+    println!("Root-post requested");
+    let msg = payload.msg;
+    println!("Message: {msg} ");
+
+    StatusCode::ACCEPTED
+}
+
+#[derive(Deserialize)]
+struct Message {
+    pub msg: String,
 }
