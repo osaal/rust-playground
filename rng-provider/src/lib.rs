4fb166a73d7b33d38494c53fcd4ff6a4d9569406
osaal <29450366+osaal@users.noreply.github.com>
2026-02-21 15:14:31 +0200

    Add basic RNG provider trait and impl

--- rng-provider/src/lib.rs
+++ rng-provider/src/lib.rs
@@ -1,1 +1,37 @@
+use std::{
+    fs::File,
+    io::{Error, Read},
+};
 
+trait RNGProvider {
+    /// Attempt to read bytes from the PRNG provider
+    ///
+    /// The return should propagate any IO errors or construct its own.
+    fn try_get_bytes(&self, buf: &mut [u8]) -> Result<(), std::io::Error>;
+}
+
+struct OdDevRandom {}
+
+impl OdDevRandom {
+    pub fn new() -> Self {
+        OdDevRandom {}
+    }
+}
+
+impl RNGProvider for OdDevRandom {
+    fn try_get_bytes(&self, buf: &mut [u8]) -> Result<(), Error> {
+        let mut handle = File::open("/dev/random")?;
+        handle.read_exact(buf)
+    }
+}
+
+mod tests {
+    use super::*;
+    #[test]
+    fn returns_something() {
+        let provider = OdDevRandom::new();
+        let mut buf = [0u8; 16];
+        let res = provider.try_get_bytes(&mut buf);
+        dbg!(res);
+    }
+}
