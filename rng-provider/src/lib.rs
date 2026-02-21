d9087beeb02f1a058addd858277ed0c130dab662
osaal <29450366+osaal@users.noreply.github.com>
2026-02-21 17:18:31 +0200

    Remove old, file system-based RNG device impl, add documentation for remaining safe impl

--- rng-provider/src/lib.rs
+++ rng-provider/src/lib.rs
@@ -1,7 +1,4 @@
-use std::{
-    fs::File,
-    io::{Error, ErrorKind, Read},
-};
+use std::io::{Error, ErrorKind};
 
 /// Synchronous RNG provider
 ///
@@ -16,47 +13,19 @@ pub trait RNGProvider {
     fn try_get_bytes(buflen: usize) -> Result<Self::RNGRawByteArray, std::io::Error>;
 }
 
-/// Retrieve random numbers from the `/dev/random` device on Unix-like systems
-pub struct UnixDevRandom {}
-
-impl RNGProvider for UnixDevRandom {
-    type RNGRawByteArray = Vec<u8>;
-
-    fn try_get_bytes(buflen: usize) -> Result<Vec<u8>, Error> {
-        match buflen {
-            val if val >= isize::MAX as usize => {
-                return Err(Error::new(
-                    ErrorKind::InvalidInput,
-                    "`buflen` must be less than `isize::MAX`",
-                ));
-            }
-            val if val == 0 => {
-                return Err(Error::new(
-                    ErrorKind::InvalidInput,
-                    "`buflen` must be non-zero",
-                ));
-            }
-            _ => (),
-        }
-
-        // TODO: This should probably be async since /dev/random blocks until it can return...
-        let mut handle = File::open("/dev/random")?;
-        let mut buf = vec![0; buflen];
-
-        match handle.read_exact(&mut buf) {
-            Ok(_) => Ok(buf),
-            Err(e) => Err(e),
-        }
-    }
-}
-
 /// Safely retrieve random numbers from the `/dev/random` device on Unix-like systems
 ///
 /// This interface uses the `getrandom(2)` syscall, resulting in a safer alternative
 /// than directly reading the file.
-pub struct UnixDevRandomSafe {}
+///
+/// However, it is **not async safe**:
+///
+/// > If the `getrandom(2)` syscall would have blocked due to issues with the byte or entropy pool,
+/// the interface will return an error. See the `DESCRIPTION` section of `man 2 getrandom` for
+/// more information.
+pub struct UnixDevRandom {}
 
-impl RNGProvider for UnixDevRandomSafe {
+impl RNGProvider for UnixDevRandom {
     type RNGRawByteArray = Vec<u8>;
 
     fn try_get_bytes(buflen: usize) -> Result<Self::RNGRawByteArray, std::io::Error> {
@@ -113,41 +82,21 @@ mod tests {
     use crate::*;
 
     #[test]
-    fn unix_dev_random() {
+    fn unix_dev_random_safe() {
         let res = UnixDevRandom::try_get_bytes(16);
         assert!(res.is_ok());
     }
 
     #[test]
-    fn unix_dev_random_safe() {
-        let res = UnixDevRandomSafe::try_get_bytes(16);
-        assert!(res.is_ok());
-    }
-
-    #[test]
-    fn overflow_udr() {
+    fn overflow_udrs() {
         let res = UnixDevRandom::try_get_bytes((isize::MAX as usize) + 1);
         assert!(res.is_err());
         assert_eq!(res.unwrap_err().kind(), ErrorKind::InvalidInput);
     }
 
     #[test]
-    fn overflow_udrs() {
-        let res = UnixDevRandomSafe::try_get_bytes((isize::MAX as usize) + 1);
-        assert!(res.is_err());
-        assert_eq!(res.unwrap_err().kind(), ErrorKind::InvalidInput);
-    }
-
-    #[test]
-    fn zero_sized_udr() {
-        let res = UnixDevRandom::try_get_bytes(0);
-        assert!(res.is_err());
-        assert_eq!(res.unwrap_err().kind(), ErrorKind::InvalidInput);
-    }
-
-    #[test]
     fn zero_sized_udrs() {
-        let res = UnixDevRandomSafe::try_get_bytes(0);
+        let res = UnixDevRandom::try_get_bytes(0);
         assert!(res.is_err());
         assert_eq!(res.unwrap_err().kind(), ErrorKind::InvalidInput);
     }
